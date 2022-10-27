a4079cfa3146e094c5f8416721e414c14606696a
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-10-27 20:03:00 +0200

    should work

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+import subprocess
 
-src = Path("{{cookiecutter.project_slug}}")
+cwd = Path.cwd()
+src = cwd / "{{cookiecutter.project_slug}}"
 # {% if cookiecutter.has_cli != "y" %}
 (src / "cli.py").unlink(missing_ok=True)
 # {% endif %}
+subprocess.check_call(["poetry", "lock"])
+subprocess.check_call(["nix", "flake", "lock"])
